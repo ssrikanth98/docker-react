5d076c0b4764fc7ec4b0b4b3b3c045794ed5d3be
ssrikanth98 <srikanthseepani@gmail.com>
2024-05-24 15:26:39 +0530

    test

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          learn react
+          learn react from feature branch
         </a>
       </header>
     </div>
